bf1de3e6a4e7c05199781075f8b7e87d1e066366
Brutesque <brutesque@users.noreply.github.com>
2018-08-27 12:57:03 +0200

    removed test for deleted method

--- tests/base/rest/test_RESTClient.py
+++ tests/base/rest/test_RESTClient.py
@@ -9,10 +9,6 @@ class TestRESTClient(TestCase):
     def test___init__(self):
         self.assertEqual(rc.host, 'httpbin.org')
 
-    def test__create_url(self):
-        response = rc._create_url(path='{command}', path_params={'command': 'get'})
-        self.assertEqual(response, 'https://httpbin.org/get')
-
     def test_nonce(self):
         response = rc.nonce()
         self.assertIsInstance(response, str)
