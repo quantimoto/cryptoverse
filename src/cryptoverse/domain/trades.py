3a725e01eddcb8ed46f0024d2275ef0999b4e59b
Brutesque <brutesque@users.noreply.github.com>
2018-09-10 06:09:10 +0200

    added repr

--- src/cryptoverse/domain/trades.py
+++ src/cryptoverse/domain/trades.py
@@ -19,6 +19,27 @@ class Trade(object):
         self.exchange = exchange
         self.pair = pair
 
+    def __repr__(self):
+        attrs = 'amount={amount}, price={price}, pair={pair}'.format(**self.as_dict())
+        return '{}({})'.format(self.__class__.__name__, attrs)
+
+    def as_dict(self):
+        result = {
+            'amount': self.amount,
+            'price': self.price,
+            'side': self.side,
+            'exchange_id': self.exchange_id,
+            'timestamp': self.timestamp,
+            'exchange': self.exchange,
+            'pair': self.pair,
+        }
+        return result
+
+    @property
+    def total(self):
+        from decimal import Decimal
+        return float(Decimal(str(self.amount)) * Decimal(str(self.price)))
+
 
 class Trades(ObjectList):
     pass
