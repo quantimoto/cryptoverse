b7e92c5da9a4d9fc86b9b1dd99d65751d68afcfd
Brutesque <brutesque@users.noreply.github.com>
2018-10-01 02:30:37 +0200

    added persistance using disk

--- src/cryptoverse/base/rest/decorators.py
+++ src/cryptoverse/base/rest/decorators.py
@@ -1,6 +1,9 @@
 import hashlib
+import json
 import logging
+import os
 import sys
+import tempfile
 import time
 from functools import wraps
 from math import floor
@@ -12,30 +15,63 @@ logger = logging.getLogger(__name__)
 
 
 class Memoize(object):
-    def __init__(self, expires):
+    def __init__(self, expires, persistent=False, instance_bound=True):
         self.expires = expires
-        self.store = {}
+        self.store = dict()
+        self.persistent = persistent
+        self.instance_bound = instance_bound
 
     def __call__(self, func):
         @wraps(func)
         def wrapper(*args, **kwargs):
+            args_str = ', '.join(['{!r}'.format(arg) for arg in args])
+            if self.instance_bound is False:
+                args_str = ', '.join(args[1:])
+            kwargs_str = ', '.join(['{}={!r}'.format(kw, arg) for kw, arg in kwargs.items()])
+
+            key = '{}.{}({})'.format(
+                func.__module__,
+                func.__name__,
+                ', '.join([x for x in [args_str, kwargs_str] if x]),
+            ).encode('UTF-8')
+
             h = hashlib.sha1()
-            key = str((args, kwargs)).encode('utf-8')
             h.update(key)
             key_hash = h.hexdigest()
 
+            filepath = os.path.join(tempfile.gettempdir(), 'cryptoverse_{}'.format(key_hash))
+
+            try:
+                cache = json.load(open(filepath, 'r'))
+            except FileNotFoundError:
+                cache = dict()
+
             response = None
+            now = time.time()
             if key_hash in self.store.keys():
-                (timestamp, response), = self.store[key_hash].items()
-                if timestamp < time.time() - self.expires:
-                    response = None
-                else:
-                    logger.debug('Returning stored response for: {}({} {})'.format(func, args, kwargs))
+                timestamp = max(self.store[key_hash].keys())
+                stored_response = self.store[key_hash][timestamp]
+                if float(timestamp) > now - self.expires:
+                    response = stored_response
+                    logger.debug('Returning stored response from memory for: {}'.format(key))
+            else:
+                timestamp = max(cache.keys())
+                stored_response = cache[timestamp]
+                if float(timestamp) > now - self.expires:
+                    response = stored_response
+                    logger.debug('Returning stored response from disk for: {}'.format(key))
 
             if response is None:
+                now = time.time()
                 response = func(*args, **kwargs)
+
+                # Store response in memory
                 self.store[key_hash] = dict()
-                self.store[key_hash][time.time()] = response
+                self.store[key_hash][now] = response
+
+                # Store response to disk
+                cache = {now: response}
+                json.dump(cache, open(filepath, 'w'))
 
             return response
 
